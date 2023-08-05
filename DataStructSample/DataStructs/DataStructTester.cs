2e0fca3a60c3c2c75154519bd9e689937a39a8e5
Andres Perez Campillo <tomailsandstuff@gmail.com>
2023-08-05 11:31:57 -0500

    Added List, Array samples

---
+++ DataStructSample/DataStructs/DataStructTester.cs
@@ -0,0 +1,86 @@
+﻿namespace DataStructSample
+{
+    public class DataStructTester
+    {
+        #region LISTS
+
+        public void IterateList(List<int> list)
+        {
+            if (list != null && list.Count > 0)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.WriteLine($"List[{i}]: {list[i]}");
+                }
+            }
+        }
+
+        public List<int> AddElements(List<int> source, int[] elements)
+        {
+            List<int> result;
+
+            if (source == null)
+            {
+                result = new List<int>();
+            }
+            else
+            {
+                result = new List<int>(source);
+            }
+
+            if (elements != null && elements.Length > 0)
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    result.Add(elements[i]);
+                }
+            }
+
+            return result;
+        }
+
+        #endregion LISTS
+
+        public void IterateList(int[] arr)
+        {
+            if (arr != null && arr.Length > 0)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine($"List[{i}]: {arr[i]}");
+                }
+            }
+        }
+
+        public int[] AddElements(int[] source, int[] elements)
+        {
+            int[] result;
+
+            if (source == null)
+            {
+                result = new int[0];
+            }
+            else
+            {
+                result = source;
+            }
+
+            if (elements != null && elements.Length > 0)
+            {
+                result = new int[source.Length + elements.Length];
+
+                for (int i = 0; i < source.Length; i++)
+                {
+                    result[i] = source[i];
+                }
+
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    result[i + source.Length] = elements[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- DataStructSample/Program.cs
+++ DataStructSample/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DataStructSample;
+
 Console.WriteLine("Hello, World!");
+
+//int[] arr = new int[4]; // Extensión
+int[] arr = { 1, 2, 3, 4 }; // Comprensión
+//int[] arr = new int[0];
+
+//List<int> list = null;
+//List<int> list = new List<int>();
+List<int> list = new List<int> { 1, 2, 3, 4 };
+//List<int> list = new List<int>(arr);
+
+int[] elements = { 4, 5, 6 };
+
+DataStructTester dataStructSample = new DataStructTester();
+
+List<int> addedList = dataStructSample.AddElements(list, elements);
+int[] addedArr = dataStructSample.AddElements(arr, elements);
+
+dataStructSample.IterateList(addedList);
+dataStructSample.IterateList(addedArr);
