14d92d450a5b2e25c63c127d7e493c96f366bc19
Andres Perez Campillo <tomailsandstuff@gmail.com>
2023-08-12 10:38:12 -0500

    Added Queue, Stack, Dict samples

--- DataStructSample/DataStructs/DataStructTester.cs
+++ DataStructSample/DataStructs/DataStructTester.cs
@@ -82,5 +82,119 @@
 
             return result;
         }
+
+        #region QUEUE
+
+        public bool IsElementInCollection(string str, Queue<string> queue)
+        {
+            bool result = false;
+
+            do
+            {
+                result = str.Equals(queue.Dequeue()); //str == queue.Dequeue();
+            }
+            while (!result && queue.Count > 0);
+
+            return result;
+        }
+
+        public void Populate(string[] strs, Queue<string> queue)
+        {
+            for (int i = 0; i < strs.Length; i++)
+            {
+                queue.Enqueue(strs[i]);
+            }
+        }
+
+        #endregion QUEUE
+
+        public bool IsElementInCollection(string str, Stack<string> stack)
+        {
+            bool result = false;
+
+            do
+            {
+                result = str.Equals(stack.Pop()); //str == stack.Pop();
+            }
+            while (!result && stack.Count > 0);
+
+            return result;
+        }
+
+        public void Populate(string[] strs, Stack<string> stack)
+        {
+            foreach (string str in strs)
+            {
+                stack.Push(str);
+            }
+        }
+
+        public Dictionary<int, string> CreateDictionary(int[] ids, string[] names)
+        {
+            Dictionary<int, string> result = new Dictionary<int, string>();
+
+            //int dictLength = Math.Min(ids.Length, names.Length);
+            //int dictLength = (ids.Length < names.Length) ? ids.Length : names.Length;
+
+            int dictLength = 0;
+
+            if (ids.Length < names.Length)
+            {
+                dictLength = ids.Length;
+            }
+            else
+            {
+                dictLength = names.Length;
+            }
+
+            for (int i = 0; i < dictLength; i++)
+            {
+                result.Add(ids[i], names[i]);
+            }
+
+            return result;
+        }
+
+        public void PrintDictionary(Dictionary<int, string> dict)
+        {
+            if (dict.Count < 1)
+            {
+                Console.WriteLine("dict is empty");
+            }
+            else
+            {
+                foreach (var registry in dict)
+                {
+                    Console.WriteLine(registry.ToString());
+                }
+            }
+        }
+
+        public bool IsValueInDictionary(string val, Dictionary<int, string> dict)
+        {
+            bool result = false;
+
+            //foreach (var key in dict.Keys)
+            //{
+            //    result = dict[key] == val;
+
+            //    if (result)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            foreach (var dictValue in dict.Values)
+            {
+                result = val == dictValue;
+
+                if (result)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
     }
 }
--- DataStructSample/Program.cs
+++ DataStructSample/Program.cs
@@ -12,12 +12,44 @@ int[] arr = { 1, 2, 3, 4 }; // Comprensión
 List<int> list = new List<int> { 1, 2, 3, 4 };
 //List<int> list = new List<int>(arr);
 
+Queue<string> queue = new Queue<string>();
+Stack<string> stack = new Stack<string>();
+
+string[] strs = { "hola", "adios", "blabla" };
+
 int[] elements = { 4, 5, 6 };
 
 DataStructTester dataStructSample = new DataStructTester();
 
-List<int> addedList = dataStructSample.AddElements(list, elements);
-int[] addedArr = dataStructSample.AddElements(arr, elements);
+//List<int> addedList = dataStructSample.AddElements(list, elements);
+//int[] addedArr = dataStructSample.AddElements(arr, elements);
 
-dataStructSample.IterateList(addedList);
-dataStructSample.IterateList(addedArr);
+//dataStructSample.IterateList(addedList);
+//dataStructSample.IterateList(addedArr);
+
+//dataStructSample.Populate(strs, queue);
+//dataStructSample.Populate(strs, stack);
+
+//Console.WriteLine($"Element {"hola"} is in queue: {dataStructSample.IsElementInCollection("hola", queue)}");
+//Console.WriteLine($"Element {"hola"} is in stack: {dataStructSample.IsElementInCollection("hola", stack)}");
+
+//Console.WriteLine($"Element {"Hola"} is in queue: {dataStructSample.IsElementInCollection("Hola", queue)}");
+//Console.WriteLine($"Element {"Hola"} is in stack: {dataStructSample.IsElementInCollection("Hola", stack)}");
+
+Dictionary<int, string> dict = dataStructSample.CreateDictionary(arr, strs);
+dataStructSample.PrintDictionary(dict);
+Console.WriteLine($"Value {"hola"} is in dict: {dataStructSample.IsValueInDictionary("hola", dict)}");
+Console.WriteLine($"Value {"Hola"} is in dict: {dataStructSample.IsValueInDictionary("Hola", dict)}");
+
+//dict.Add(1, "cualquierotracosa");
+dict.TryAdd(1, "cualquierotracosa");
+dataStructSample.PrintDictionary(dict);
+
+dict.Remove(1);
+dataStructSample.PrintDictionary(dict);
+
+dict.TryAdd(1, "cualquierotracosa");
+dataStructSample.PrintDictionary(dict);
+
+dict.Clear();
+dataStructSample.PrintDictionary(dict);
