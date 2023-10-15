fd2bcf5591f29ec55da390c1e4f52eb63d8e1c74
Andres Perez Campillo <tomailsandstuff@gmail.com>
2023-10-15 13:13:47 -0500

    Update FactoryFacade.cs

--- UnityDemo/Patterns_Demo/Assets/Scripts/FactoryFacade.cs
+++ UnityDemo/Patterns_Demo/Assets/Scripts/FactoryFacade.cs
@@ -36,14 +36,4 @@ public class FactoryFacade : MonoBehaviour
             Destroy(this);
         }
     }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
 }
