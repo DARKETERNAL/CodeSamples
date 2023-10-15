8f22b5563d73eddfc0f83197ac5e4c519e6aabff
Andres Perez Campillo <tomailsandstuff@gmail.com>
2023-10-15 13:06:46 -0500

    ObjectPool derives from AbstractPool

--- UnityDemo/Patterns_Demo/Assets/Scripts/ObjectPool.cs
+++ UnityDemo/Patterns_Demo/Assets/Scripts/ObjectPool.cs
@@ -1,7 +1,6 @@
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public class ObjectPool : AbstractObjectPool<Object>
 {
     public enum EPoolType
     {
@@ -11,57 +10,7 @@ public class ObjectPool : MonoBehaviour
     }
 
     [SerializeField]
-    private int poolSize = 5;
+    private EPoolType poolType;
 
-    [SerializeField]
-    private EPoolType poolType = EPoolType.None;
-
-    private List<Object> pool = new List<Object>();
-
-    public EPoolType PoolType => poolType;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        if (FactoryFacade.Instance != null)
-        {
-            for (int i = 0; i < poolSize; i++)
-            {
-                CreateObjectInstance();
-            }
-        }
-    }
-
-    private void CreateObjectInstance()
-    {
-        Object objInstance = FactoryFacade.Instance.CreateInstance(PoolType);
-        //objInstance.Pool = this;
-        Recycle(objInstance);
-    }
-
-    public Object Retrieve()
-    {
-        Object result = null;
-
-        if (pool.Count < 1)
-        {
-            CreateObjectInstance();
-        }
-
-        result = pool[0];
-        pool.RemoveAt(0);
-        result.transform.parent = null;
-        result.Reset(true);
-
-        return result;
-    }
-
-    public void Recycle(Object obj)
-    {
-        obj.Reset(false);
-        obj.transform.parent = transform;
-        obj.transform.position = transform.position;
-        obj.transform.rotation = transform.rotation;
-        pool.Add(obj);
-    }
+    public override EPoolType PoolType => poolType;
 }
